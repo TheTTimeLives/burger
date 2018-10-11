5b67b0f2e41ea115e372e9a17fbaac98c27aee52
TheTTimeLives <37144865+TheTTimeLives@users.noreply.github.com>
2018-10-11 07:17:21 -0400

    Added JawsDB

--- config/connections.js
+++ config/connections.js
@@ -3,14 +3,20 @@
 // const mysql = require('mysql');
 
 var mysql = require('mysql');
+var connect;
 
-var connect = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    port: 3306,
-    database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+ connect = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connect = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        port: 3306,
+        database: 'burgers_db'
+    });
+};
+
 
 connect.connect(function(err){
     if (err) throw err;
